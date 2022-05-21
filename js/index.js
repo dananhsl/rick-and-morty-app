c4b27b8b6af2ff5c741ccc5b3ac3eee3af32189f
Laura Kesel <laura.kesel@gmail.com>
2022-05-21 19:37:08 +0200

    temporary solution for bookmarks

--- js/index.js
+++ js/index.js
@@ -1,74 +1,9 @@
-//import { renderCards } from './renderCards'
-//import 'fetchData.js'
-const cardsContainer = document.querySelector('[data-js="cards"]')
-const form = document.querySelector('[data-js="form"]')
-/*const filterForm = document.querySelector('[data-js=filter-form]')
- ^ WE NEED TO WORK ON THIS */
+/* -----------------------------------Global Variables----------------------------------------------------- */
+const cardsContainer = document.querySelector('[data-js="cards"]');
+const favoritesContainer = document.querySelector('[data-js="favoriteCards"]');
 
-/*let currentFilter = 'all';*/
-let allCards = []
 
-/*filterForm.addEventListener('change', () => {
-  currentFilter = filterForm.elements['tag-filter'].value;
-  renderCards();
-})*/
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-
-  const fighterNameElement = form.elements.name
-  const statusElement = form.elements.status
-  const speciesElement = form.elements.species
-  const locationElement = form.elements.location
-  const imageElement = form.elements.image
-
-  const newCard = {
-    name: fighterNameElement.value,
-    status: statusElement.value,
-    species: speciesElement.value,
-    location: locationElement.value,
-    image: imageElement.value,
-    isBookmarked: false,
-  }
-
-  allCards = [newCard, ...allCards]
-  form.reset()
-  fighterNameElement.focus()
-  renderCards(allCards)
-})
-
-function renderCards(allCards) {
-  cardsContainer.innerHTML = ''
-
-  allCards
-    //.filter(card => card.tags.includes(currentFilter) || currentFilter === 'all')
-    .forEach((card) => {
-      const cardElement = document.createElement('li')
-      cardElement.className = 'card'
-      cardElement.innerHTML = `
-      <section class="characterCard" data-js="epicFighterCard">
-      <img src="${card.image}" />
-  
-      <button class="bookmarkBtn" data-js="bookmark">Click me</button>
-  
-      <label class="nameLabel">${card.name}</label>
-  
-      <label class="statusLabel">${card.status}</label>
-  
-      <label class="speciesLabel">${card.species}</label>
-  
-      <label class="locationLabel">${card.location.name}</label>
-    </section>
-      `
-      cardsContainer.append(cardElement)
-      const bookmarkElement = cardElement.querySelector('[data-js="bookmark"]')
-      bookmarkElement.addEventListener('click', () => {
-        card.isBookmarked = !card.isBookmarked
-        bookmarkElement.classList.toggle('card__bookmark--active')
-      })
-    })
-}
-
+/* -----------------------------------Fetch objects, save in list and render------------------------------- */
 const baseUrl = 'https://rickandmortyapi.com/api/character?page='
 const numPages = 5
 
@@ -77,9 +12,108 @@ const urls = Array(numPages)
   .map((_, index) => baseUrl + (index + 1))
 
 const promises = urls.map((url) => fetch(url).then((res) => res.json()))
+let allCards = [];
 
 Promise.all(promises).then((pages) => {
   allCards = pages.flatMap((page) => page.results)
-  renderCards(allCards)
-  console.log(allCards)
+  allCards = allCards.map((card) => {
+      return {...card, isBookmarked: false};
+  })
+  allCards.forEach((card) => {
+      cardsContainer.append(renderCard(card));
+  })
 })
+
+
+/* -----------------------------------Single Page Application---------------------------------------------- */
+document.querySelector('[data-js="homebtn"]').addEventListener('click', () => {
+    document.querySelector('[data-js="cards"]').classList.remove('hidden');
+    document.querySelector('[data-js="favoriteCards"]').classList.add('hidden');
+    document.querySelector('[data-js="createCards"]').classList.add('hidden');
+  });
+
+document.querySelector('[data-js="bookmarkbtn"]').addEventListener('click', () => {
+    document.querySelector('[data-js="cards"]').classList.add('hidden');
+    document.querySelector('[data-js="favoriteCards"]').classList.remove('hidden');
+    document.querySelector('[data-js="createCards"]').classList.add('hidden');
+    const bookmarkedCards = allCards.filter(card => {
+      return card.isBookmarked === true;
+    });
+    renderFavorites(bookmarkedCards);
+  });
+
+document.querySelector('[data-js="createBtn"]').addEventListener('click', () => {
+    document.querySelector('[data-js="cards"]').classList.add('hidden');
+    document.querySelector('[data-js="favoriteCards"]').classList.add('hidden');
+    document.querySelector('[data-js="createCards"]').classList.remove('hidden');
+  });
+
+
+/* ------------------------------------Render homepage cards------------------------------------------------*/
+function renderCard(card) {
+    cardsContainer.innerHTML = ''
+      //.filter(card => card.tags.includes(currentFilter) || currentFilter === 'all')
+      
+        const cardElement = document.createElement('li')
+        cardElement.className = 'card'
+        cardElement.innerHTML = `
+        <section class="characterCard" data-js="epicFighterCard">
+        <img src="${card.image}" />
+    
+        <button class="bookmarkBtn" data-js="bookmark">Click me</button>
+    
+        <label class="nameLabel">${card.name}</label>
+    
+        <label class="statusLabel">${card.status}</label>
+    
+        <label class="speciesLabel">${card.species}</label>
+    
+        <label class="locationLabel">${card.location.name}</label>
+      </section>
+        `
+
+        const bookmarkElement = cardElement.querySelector('[data-js="bookmark"]')
+        bookmarkElement.addEventListener('click', () => {
+          card.isBookmarked = !card.isBookmarked
+          console.log(card.isBookmarked);
+          bookmarkElement.classList.toggle('card__bookmark--active')
+        })  
+  }
+
+
+/* ------------------------------------Render bookmark cards----------------------------------------------- */
+  function renderFavorites(bookmarkedCards) {
+    favoritesContainer.innerHTML = ''
+  
+    bookmarkedCards
+      //.filter(card => card.tags.includes(currentFilter) || currentFilter === 'all')
+      .forEach((bookmarkedCard) => {
+        const cardElement = document.createElement('li')
+        cardElement.className = 'card'
+        cardElement.innerHTML = `
+        <section class="characterCard" data-js="epicFighterCard">
+        <img src="${bookmarkedCard.image}" />
+    
+        <button class="bookmarkBtn" data-js="bookmark">Click me</button>
+    
+        <label class="nameLabel">${bookmarkedCard.name}</label>
+    
+        <label class="statusLabel">${bookmarkedCard.status}</label>
+    
+        <label class="speciesLabel">${bookmarkedCard.species}</label>
+    
+        <label class="locationLabel">${bookmarkedCard.location.name}</label>
+      </section>
+        `
+        favoritesContainer.append(cardElement)
+
+        const bookmarkElement = cardElement.querySelector('[data-js="bookmark"]')
+        bookmarkElement.addEventListener('click', () => {
+            bookmarkedCard.isBookmarked = !bookmarkedCard.isBookmarked;
+            bookmarkElement.classList.toggle('card__bookmark--active')
+            console.log(bookmarkedCard.isBookmarked);
+          })
+          
+      })  
+      
+  }
---
+++ js/index_original.js
@@ -0,0 +1,90 @@
+//import { renderCards } from './renderCards'
+//import 'fetchData.js'
+const cardsContainer = document.querySelector('[data-js="cards"]') //section, die die cards beinhaltet
+const form = document.querySelector('[data-js="form"]') //Formular
+/*const filterForm = document.querySelector('[data-js=filter-form]')
+ ^ WE NEED TO WORK ON THIS */
+
+/*let currentFilter = 'all';*/
+let allCards = [] //leere Liste
+
+
+/*filterForm.addEventListener('change', () => {
+  currentFilter = filterForm.elements['tag-filter'].value;
+  renderCards();
+})*/
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const fighterNameElement = form.elements.name
+  const statusElement = form.elements.status
+  const speciesElement = form.elements.species
+  const locationElement = form.elements.location
+  const imageElement = form.elements.image
+
+  const newCard = {
+    name: fighterNameElement.value,
+    status: statusElement.value,
+    species: speciesElement.value,
+    location: locationElement.value,
+    image: imageElement.value,
+    isBookmarked: false,
+  }
+
+  allCards = [newCard, ...allCards] //leere Liste wird mit neuen Objekten und vorhandenen Objekten befüllt
+  form.reset()
+  fighterNameElement.focus()
+  renderCards(allCards)
+})
+
+function renderCards(allCards) {
+  cardsContainer.innerHTML = ''
+
+  allCards
+    //.filter(card => card.tags.includes(currentFilter) || currentFilter === 'all')
+    .forEach((card) => {
+      const cardElement = document.createElement('li')
+      cardElement.className = 'card'
+      cardElement.innerHTML = `
+      <section class="characterCard" data-js="epicFighterCard">
+      <img src="${card.image}" />
+  
+      <button class="bookmarkBtn" data-js="bookmark">Click me</button>
+  
+      <label class="nameLabel">${card.name}</label>
+  
+      <label class="statusLabel">${card.status}</label>
+  
+      <label class="speciesLabel">${card.species}</label>
+  
+      <label class="locationLabel">${card.location.name}</label>
+    </section>
+      `
+      cardsContainer.append(cardElement)
+
+      const bookmarkElement = cardElement.querySelector('[data-js="bookmark"]')
+      bookmarkElement.addEventListener('click', () => {
+        card.isBookmarked = !card.isBookmarked
+        bookmarkElement.classList.toggle('card__bookmark--active')
+      })
+    })  
+}
+
+const baseUrl = 'https://rickandmortyapi.com/api/character?page='
+const numPages = 5
+
+const urls = Array(numPages)
+  .fill() // [undefined, undefined, ...]
+  .map((_, index) => baseUrl + (index + 1))
+
+const promises = urls.map((url) => fetch(url).then((res) => res.json()))
+
+Promise.all(promises).then((pages) => {
+  allCards = pages.flatMap((page) => page.results)
+  allCards.forEach(card => {
+    card.isBookmarked = false;
+  })
+  renderCards(allCards)
+  console.log(allCards)
+})
