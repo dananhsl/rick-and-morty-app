1a6eb6cdba0a94247d836008ecf5f3e1a8668635
Dana Neuhäusel <skadi@Danas-MacBook-Air.local>
2022-05-22 13:06:07 +0200

    Hotfix 1.0

--- js/index.js
+++ js/index.js
@@ -1,18 +1,35 @@
 //import { renderCards } from './renderCards'
 //import 'fetchData.js'
 const cardsContainer = document.querySelector('[data-js="cards"]') //section, die die cards beinhaltet
-const form = document.querySelector('[data-js="form"]') //Formular
+const form = document.querySelector('[data-js="form"]')
+const favoritesContainer = document.querySelector('[data-js="favoriteCards"]') //Formular
 /*const filterForm = document.querySelector('[data-js=filter-form]')
  ^ WE NEED TO WORK ON THIS */
 
 /*let currentFilter = 'all';*/
 let allCards = [] //leere Liste
 
-
 /*filterForm.addEventListener('change', () => {
   currentFilter = filterForm.elements['tag-filter'].value;
   renderCards();
 })*/
+
+const baseUrl = 'https://rickandmortyapi.com/api/character?page='
+const numPages = 5
+
+const urls = Array(numPages)
+  .fill() // [undefined, undefined, ...]
+  .map((_, index) => baseUrl + (index + 1))
+
+const promises = urls.map((url) => fetch(url).then((res) => res.json()))
+
+Promise.all(promises).then((pages) => {
+  allCards = pages.flatMap((page) => page.results)
+  allCards.forEach((card) => {
+    card.isBookmarked = false
+  })
+  renderHome(allCards)
+})
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
@@ -40,28 +57,33 @@ form.addEventListener('submit', (event) => {
 
 /* -----------------------------------Single Page Application---------------------------------------------- */
 document.querySelector('[data-js="homebtn"]').addEventListener('click', () => {
-  document.querySelector('[data-js="cards"]').classList.remove('hidden');
-  document.querySelector('[data-js="favoriteCards"]').classList.add('hidden');
-  document.querySelector('[data-js="createCards"]').classList.add('hidden');
-  renderHome(allCards);
-});
+  document.querySelector('[data-js="cards"]').classList.remove('hidden')
+  document.querySelector('[data-js="favoriteCards"]').classList.add('hidden')
+  document.querySelector('[data-js="createCards"]').classList.add('hidden')
+  renderHome(allCards)
+})
 
-document.querySelector('[data-js="bookmarkbtn"]').addEventListener('click', () => {
-  document.querySelector('[data-js="cards"]').classList.add('hidden');
-  document.querySelector('[data-js="favoriteCards"]').classList.remove('hidden');
-  document.querySelector('[data-js="createCards"]').classList.add('hidden');
-  const bookmarkedCards = allCards.filter(card => {
-    return card.isBookmarked === true;
-  });
-  renderFavorites(bookmarkedCards);
-});
+document
+  .querySelector('[data-js="bookmarkbtn"]')
+  .addEventListener('click', () => {
+    document.querySelector('[data-js="cards"]').classList.add('hidden')
+    document
+      .querySelector('[data-js="favoriteCards"]')
+      .classList.remove('hidden')
+    document.querySelector('[data-js="createCards"]').classList.add('hidden')
+    const bookmarkedCards = allCards.filter((card) => {
+      return card.isBookmarked === true
+    })
+    renderFavorites(bookmarkedCards)
+  })
 
-document.querySelector('[data-js="createBtn"]').addEventListener('click', () => {
-  document.querySelector('[data-js="cards"]').classList.add('hidden');
-  document.querySelector('[data-js="favoriteCards"]').classList.add('hidden');
-  document.querySelector('[data-js="createCards"]').classList.remove('hidden');
-});
-
+document
+  .querySelector('[data-js="createBtn"]')
+  .addEventListener('click', () => {
+    document.querySelector('[data-js="cards"]').classList.add('hidden')
+    document.querySelector('[data-js="favoriteCards"]').classList.add('hidden')
+    document.querySelector('[data-js="createCards"]').classList.remove('hidden')
+  })
 
 /* ------------------------------------Render all cards on homepage------------------------------------------------*/
 function renderHome(allCards) {
@@ -91,14 +113,14 @@ function renderHome(allCards) {
       cardsContainer.append(cardElement)
 
       const bookmarkElement = cardElement.querySelector('[data-js="bookmark"]')
-      if (card.isBookmarked )
-        bookmarkElement.classList.toggle('card__bookmark--active');
-      
+      if (card.isBookmarked)
+        bookmarkElement.classList.toggle('card__bookmark--active')
+
       bookmarkElement.addEventListener('click', () => {
         card.isBookmarked = !card.isBookmarked
         bookmarkElement.classList.toggle('card__bookmark--active')
       })
-    })  
+    })
 }
 
 /* ------------------------------------Render bookmarked cards----------------------------------------------- */
@@ -111,51 +133,34 @@ function renderFavorites(bookmarkedCards) {
       cardElement.className = 'card'
       cardElement.innerHTML = `
       <section class="characterCard" data-js="epicFighterCard">
-      <img src="${bookmarkedCard.image}" />
+      
+      <img class="card__image"src="${bookmarkedCard.image}" />
   
-      <button class="bookmarkBtn card__bookmark--active" data-js="bookmark">Click me</button>
-  
-      <label class="nameLabel">${bookmarkedCard.name}</label>
-  
-      <label class="statusLabel">${bookmarkedCard.status}</label>
+      <button class="card__bookmark card__bookmark--active" data-js="bookmark"></button>
+      <div class="card__text">
+      <label class="nameLabel">Name: ${bookmarkedCard.name}</label>
+      <div class="card__flex"
+      <label class="statusLabel"> ${bookmarkedCard.status} &#183; </label>
   
       <label class="speciesLabel">${bookmarkedCard.species}</label>
-  
-      <label class="locationLabel">${bookmarkedCard.location.name}</label>
+      </div>
+      <label class="locationLabel">Last seen Location: ${bookmarkedCard.location.name}</label>
+      </div>
     </section>
       `
       favoritesContainer.append(cardElement)
-      
+
       const bookmarkElement = cardElement.querySelector('[data-js="bookmark"]')
       bookmarkElement.addEventListener('click', () => {
         bookmarkElement.classList.toggle('card__bookmark--active')
-        
-        allCards = allCards.map((allCard => {
-          if (allCard.id === bookmarkedCard.id){
-            return {...allCard, isBookmarked: !allCard.isBookmarked};
-          }else {
-            return allCard;
+
+        allCards = allCards.map((allCard) => {
+          if (allCard.id === bookmarkedCard.id) {
+            return { ...allCard, isBookmarked: !allCard.isBookmarked }
+          } else {
+            return allCard
           }
-        }))
+        })
       })
-    })  
-    
+    })
 }
-
-const baseUrl = 'https://rickandmortyapi.com/api/character?page='
-const numPages = 5
-
-const urls = Array(numPages)
-  .fill() // [undefined, undefined, ...]
-  .map((_, index) => baseUrl + (index + 1))
-
-const promises = urls.map((url) => fetch(url).then((res) => res.json()))
-
-Promise.all(promises).then((pages) => {
-  allCards = pages.flatMap((page) => page.results)
-  allCards.forEach(card => {
-    card.isBookmarked = false;
-  })
-  renderCards(allCards)
-  console.log(allCards)
-})
